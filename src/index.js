85d18d234efc56d0829cd2be8c2e74424ff9c667
jackelin perez <jmperezchahua@gmail.com>
2022-01-21 20:12:07 -0500

    styles and loader

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import getApi from "./data/index.js";
 import { url } from "./util/index.js";
-import { Card } from "./ui/index.js";
+import { Cards } from "./ui/index.js";
 import { buttonEvents } from "./components/index.js";
 
 const cards = document.getElementById("countries");
+const loader = document.getElementById("loader");
 
 const getData = async (url) => {
   try {
     const data = await getApi(url);
-    cards.innerHTML = Card(data);
+    loader.style.display = "none";
+    cards.innerHTML = Cards(data);
     buttonEvents(data);
   } catch (err) {
     console.error(err);
